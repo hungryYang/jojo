ca1ba1312ab907d3bdfed598d452623b24f76aac
hungryyang <yangchengrun@kingsoft.com>
2018-09-14 15:26:49 +0800

    push

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Button from './botton'
+
+Vue.component('jo-button', Button)
+
+new Vue({
+  el: '#app'
+})
---
+++ src/botton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="jo-button">按钮</button>
+</template>
+<script>
+  export default {}
+</script>
+<style lang="scss">
+.jo-button { 
+  vertical-align: middle;
+  height: var(--button-height); 
+  padding: 0 1em; 
+  font: inherit; 
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color); 
+  background: var(--button-bg);
+  &:hover { 
+    border-color: var(--border-color-hover); 
+    position: relative; 
+    z-index: 1;
+  }
+  &:active { 
+    background: var(--button-active-bg); 
+  }
+  &:focus { 
+    outline: none; 
+  }
+}
+</style>
