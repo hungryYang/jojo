34bc8a5f1abe0b04eec43d4c84a9b9604386cf72
hungryyang <yangchengrun@kingsoft.com>
2018-09-18 22:19:23 +0800

    完成button-group

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './botton'
 import Icon from './icon'
+import ButtonGroup from './button-group'
 
 Vue.component('jo-button', Button)
 Vue.component('jo-icon', Icon)
+Vue.component('jo-button-group', ButtonGroup)
 
 new Vue({
   el: '#app',
---
+++ src/button-group.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="button-group">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    mounted () {
+      let nodes = this.$el.children
+      for (let node of nodes) {
+        if (node.nodeName.toLowerCase() !== 'button') {
+          console.warn('jo-button-group子元素应全为jo-button')
+        }
+      }
+    }
+  }
+</script>
+<style lang="scss">
+.button-group {
+  display: inline-flex;
+  vertical-align: middle;
+  border-radius: var(--border-radius);
+  .jo-button {
+    border-radius: 0;
+    &:not(:first-child) {
+      margin-left: -1px;
+    }
+    &:first-child {
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+    &:last-child {
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+  }
+}
+</style>
