b8e71338f989525206f488e2df68c05f3261f7da
hungryyang <hungryyangc@163.com>
2018-10-09 16:43:40 +0800

    单元测试用例

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
+import chai from 'chai'
 import Button from './botton'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 
+const expect = chai.expect
 Vue.component('jo-button', Button)
 Vue.component('jo-icon', Icon)
 Vue.component('jo-button-group', ButtonGroup)
@@ -15,3 +17,16 @@ new Vue({
     }
   }
 })
+
+{
+  let Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'setting'
+    }
+  })
+
+  button.$mount('#test')
+  let useElement = button.$el.querySelector('use')
+  expect(useElement.getAttribute('xlink:href')).to.equal('#i-setting')
+}
