330db231c9c2982ec44782c1c9841014607c7e89
hungryYang <ycr1714336458@163.com>
2018-10-11 17:06:31 +0800

    记录一下

--- test/button.test.js
+++ test/button.test.js
@@ -4,10 +4,7 @@ import Button from '../src/button'
 
 Vue.config.productionTip = false
 Vue.config.devtools = false
-/**
- *  BDD
- *  TDD
- * */
+
 describe('Button', () => {
   it('存在.', () => {
     expect(Button).to.be.ok
